cd0e8e099d553ef6cab8ba658981fc90ffaac9ea
Roberta Munive <ramunive@gmail.com>
2020-02-07 20:50:37 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,9 @@
+//var array = [];
+//array.length = 25;
+
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    array[i] = `I am ${i} strange loop${i === 0 ? '' : 's'}.`;
+  }
+  return array;
+}
