af11558cb392739869b3ab948668abe3641ed2ab
Roberta Munive <ramunive@gmail.com>
2020-02-07 21:16:24 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,18 +2,8 @@
 //array.length = 25;
 
 function forLoop(array) {
-  //for(var i = 0; i < 25; i++) {
-    //array[i] = `I am ${i} strange loop${i === 0 ? '' : 's'}.`;
-  //}
-  for(let i = 0; i < 25; i++){
-    if(i === 1){
-      array[i] = "I am 1 strange loop.";
-    }
-    else {
-      array[i] = `I am ${i} strange loops.`;
-    }
-
+  for(var i = 0; i < 25; i++) {
+    array[i] = `I am ${i} strange loop${i === 0 ? '' : 's'}.`;
   }
-
   return array;
 }
